df9f01b625ec2b7fd699e52628395bd0be9c49b8
BrunaLopesGois <bruna-chan@live.com>
2021-10-20 14:10:18 -0300

    feat: create GET routes

--- src/Controller/MedicosController.php
+++ src/Controller/MedicosController.php
@@ -4,12 +4,13 @@ namespace App\Controller;
 
 use App\Entity\Medico;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class MedicosController
+class MedicosController extends AbstractController
 {
     private EntityManagerInterface $entityManager;
     
@@ -21,7 +22,7 @@ class MedicosController
     /**
      * @Route("/medicos", methods={"POST"})
      */
-    public function createAction(Request $request): Response
+    public function create(Request $request): Response
     {
         $corpoRequisicao = $request->getContent();
         $dadosEmJson = json_decode($corpoRequisicao);
@@ -34,5 +35,33 @@ class MedicosController
         $this->entityManager->flush();
 
         return new JsonResponse($medico, 201);
+    }
+
+    /**
+     * @Route("/medicos", methods={"GET"})
+     */
+    public function index(): Response
+    {
+        $repositorioDeMedicos = $this->getDoctrine()->getRepository(Medico::class);
+
+        $medicoList = $repositorioDeMedicos->findAll();
+
+        return new JsonResponse($medicoList);
+    }
+
+    /**
+     * @Route("/medicos/{id}", methods={"GET"})
+     */
+    public function show(Request $request): Response
+    {
+        $id = $request->get('id');
+        $repositorioDeMedicos = $this->getDoctrine()->getRepository(Medico::class);
+
+        $medico = $repositorioDeMedicos->find($id);
+        if (is_null($medico)) {
+            return new JsonResponse('', 204);
+        }
+
+        return new JsonResponse($medico);
     }
 }
