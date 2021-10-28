0adbedb6caff1a2db28d6b4e576807e3a4b308dc
BrunaLopesGois <bruna-chan@live.com>
2021-10-28 13:27:39 -0300

    feat: especialidade resource

--- migrations/Version20211019172504.php
+++ migrations/Version20211019172504.php
@@ -1,31 +1,33 @@
 <?php
 
-declare(strict_types=1);
+// migration para sqlite (em desuso)
 
-namespace DoctrineMigrations;
+// declare(strict_types=1);
 
-use Doctrine\DBAL\Schema\Schema;
-use Doctrine\Migrations\AbstractMigration;
+// namespace DoctrineMigrations;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
-final class Version20211019172504 extends AbstractMigration
-{
-    public function getDescription(): string
-    {
-        return '';
-    }
+// use Doctrine\DBAL\Schema\Schema;
+// use Doctrine\Migrations\AbstractMigration;
 
-    public function up(Schema $schema): void
-    {
-        // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('CREATE TABLE medico (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, crm INTEGER NOT NULL, nome VARCHAR(255) NOT NULL)');
-    }
+// /**
+//  * Auto-generated Migration: Please modify to your needs!
+//  */
+// final class Version20211019172504 extends AbstractMigration
+// {
+//     public function getDescription(): string
+//     {
+//         return '';
+//     }
 
-    public function down(Schema $schema): void
-    {
-        // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('DROP TABLE medico');
-    }
-}
+//     public function up(Schema $schema): void
+//     {
+//         // this up() migration is auto-generated, please modify it to your needs
+//         $this->addSql('CREATE TABLE medico (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, crm INTEGER NOT NULL, nome VARCHAR(255) NOT NULL)');
+//     }
+
+//     public function down(Schema $schema): void
+//     {
+//         // this down() migration is auto-generated, please modify it to your needs
+//         $this->addSql('DROP TABLE medico');
+//     }
+// }
