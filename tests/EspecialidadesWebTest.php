e72383e2368cbc2275ac608117d197f03516611b
BrunaLopesGois <bruna-chan@live.com>
2021-12-07 14:59:22 -0300

    feat: functional tests

--- tests/EspecialidadesWebTest.php
+++ tests/EspecialidadesWebTest.php
@@ -7,32 +7,59 @@ use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
 
 class EspecialidadesWebTest extends WebTestCase
 {
+    private KernelBrowser $client;
+
+    private string $token;
+
+    public function setUp(): void
+    {
+        $this->client = static::createClient();
+        $this->token = $this->login($this->client);
+    }
+    
     public function testGaranteQueAutenticacaoFalhaSemRequisicao(): void
     {
-        static::createClient()->request('GET', '/especialidades');
+        $this->client->request('GET', '/especialidades');
 
         $this->assertResponseStatusCodeSame(401);
     }
 
     public function testGaranteQueEspecialidadesSaoListadas(): void
     {
-        $client = static::createClient();
-        $token = $this->login($client);
-        $client->request('GET', '/especialidades', [], [], [
-            'HTTP_AUTHORIZATION' => "Bearer $token"
+        $this->client->request('GET', '/especialidades', [], [], [
+            'HTTP_AUTHORIZATION' => "Bearer $this->token"
         ]);
+
+        $this->assertResponseIsSuccessful();
+    }
+
+    public function testInsereEspecialidade(): void
+    {
+        $this->client->request('POST', '/especialidades', [], [], [
+            'CONTENT_TYPE' => 'application/json',
+            'HTTP_AUTHORIZATION' => "Bearer $this->token"
+        ], json_encode([
+            'descricao' => 'Teste'
+        ]));
 
         $this->assertResponseIsSuccessful();
     }
 
     private function login(KernelBrowser $client): string
     {
-        $client->request('POST', '/login', [], [], [
-            'CONTENT_TYPE' => 'application/json'
-        ], json_encode([
-            'usuario' => 'username',
-            'senha' => '123456'
-        ]));
+        $client->request(
+            'POST',
+            '/login',
+            [],
+            [],
+            [
+                'CONTENT_TYPE' => 'application/json'
+            ],
+            json_encode([
+                'usuario' => 'usuario',
+                'senha' => '123456'
+            ])
+        );
         
         return json_decode($client->getResponse()->getContent())->access_token;
     }
